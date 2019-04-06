1965354a32c9c515cae2aefb377b8ad2cec5f25a
Subhankar Panda <s1panda@ucsd.edu>
2019-04-05 17:48:50 -0700

    Removed Polling

--- server/index.js
+++ server/index.js
@@ -1,27 +1,62 @@
 require('dotenv').config();
+
 const express = require('express');
 const fs = require('fs');
-const {SCHEDULE_FILE, ANNOUNCEMENT_FILE} = require('./shared');
+const Bottleneck = require('bottleneck');
+const Airtable = require('airtable');
+const moment = require('moment');
+const TIME_LIMIT = 1000 / 5;
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_KEY })
+                .base('appY4r7VAncjUupmp')
+
+const getSchedule =  () => {
+    return base('Schedule')
+            .select()
+            .all()
+            .then(records => records.map(r => r._rawJson.fields))
+            .then(records => records.map((r) => ({
+                ...r, 
+                duration: moment(r.endTime).diff(moment(r.startTime), 'hours', true)
+            })))
+            .then(records => records.sort((a, b) => new Date(a.startTime) - new Date(b.startTime)))
+}
+
+const getAnnouncements =  () => {
+    return base('Announcements')
+            .select()
+            .all()
+            .then(records => records.map(r => r._rawJson.fields))
+}
+
+//4 requests per second
+const limiter = new Bottleneck({
+    minTime: TIME_LIMIT,
+    maxConcurrent: 1
+});
+
 
 const port = process.env.port || 8000;
 const app = express()
 
 app.get('/api/schedule', (req, res) => {
-    fs.readFile(__dirname + SCHEDULE_FILE, (err, data) => {
-        res.json(
-            JSON.parse(data.toString())
-        )
-    })
+    limiter.schedule(() => getSchedule())
+        .then((r) => {
+            res.json(r)
+        })
+        .catch(e => {
+            console.error(e)
+        })
 })
 
 app.get('/api/announcements', (req, res) => {
-    fs.readFile(__dirname + ANNOUNCEMENT_FILE, (err, data) => {
-        res.json(
-            JSON.parse(data.toString())
-        )
-    })
+    limiter.schedule(() => getAnnouncements())
+        .then((r) => {
+            res.json(r)
+        })
+        .catch(e => {
+            console.error(e)
+        })
 })
-
-require('./poller');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
